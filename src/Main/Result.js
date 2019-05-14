5d9d0ea9148e6de4a6b1445e9e58862fb8c03a35
Rails Udemy Course <zonghangoh@gmail.com>
2019-05-15 01:45:36 +0800

    final

--- src/Main/Result.js
+++ src/Main/Result.js
@@ -1,9 +1,25 @@
 import React from 'react';
+const axios = require('axios')
 
-function Result(props) {
+class Result extends React.Component {
 
-    const depressCompare = (score) => {
-        if (props.malay) {
+    state = {email: ""} 
+
+    sendResult = () => {
+        // axios.get('/',)
+        axios.get('https://joni-author-api.herokuapp.com/thoughtfull', {
+            // axios.get('http://localhost:3001/thoughtfull', {
+                params: {
+                    depressScore: this.depression(),
+                    stressScore: this.stress(),
+                    anxietyScore: this.anxiety(),
+                    email: this.state.email
+                }
+        }) 
+    }
+
+    depressCompare = (score) => {
+        if (this.props.malay) {
             if (score>0 && score<=5) {
                 return <b>Normal</b>
             } else if (score>5 && score<=7) {
@@ -30,8 +46,8 @@ function Result(props) {
         }
     }
 
-    const stressCompare = (score) => {
-        if (props.malay){
+    stressCompare = (score) => {
+        if (this.props.malay){
             if (score>0 && score<=4) {
                 return <b>Normal</b>
             } else if (score>4 && score<=6) {
@@ -58,8 +74,8 @@ function Result(props) {
         }
     }
 
-    const anxietyCompare = (score) => {
-        if (props.malay) {
+    anxietyCompare = (score) => {
+        if (this.props.malay) {
             if (score>0 && score<=7) {
                 return <b>Normal</b>
             } else if (score>7 && score<=9) {
@@ -88,44 +104,46 @@ function Result(props) {
     }
 
 
-    const depression = () => {
+    depression = () => {
         let depressScore = [true, true, false, true, false, true, true, true, true, false, true, true, false, true, true, false, false, true, true, true, false]
         let score = 0;
         for (let i=0; i<depressScore.length; i++) {
-            if (!depressScore[i] && props.result[i]) {
-                score = score + props.result[i]
+            if (!depressScore[i] && this.props.result[i]) {
+                score = score + this.props.result[i]
             }
         }
         return score;
     }
-    const stress = () => {
+    stress = () => {
         let stressScore = [false, true, true, true, true, false, true, false, true, true, false, false, true, false, true, true, true, false, true, true, true]
         let score = 0;
         for (let i=0; i<stressScore.length; i++) {
-            if (!stressScore[i] && props.result[i]) {
-                score = score + props.result[i]
+            if (!stressScore[i] && this.props.result[i]) {
+                score = score + this.props.result[i]
             }
         }
         return score;
     }
-    const anxiety = () => {
+    anxiety = () => {
         let anxietyScore = [true, false, true, false, true, true, false, true, false, true, true, true, true, true, false, true, true, true, false, false, true]
         let score = 0;
         for (let i=0; i<anxietyScore.length; i++) {
-            if (!anxietyScore[i] && props.result[i]) {
-                score = score + props.result[i]
+            if (!anxietyScore[i] && this.props.result[i]) {
+                score = score + this.props.result[i]
             }
         }
         return score;
     }
 
-    if (props.malay) {
+    render() {
+
+    if (this.props.malay) {
         return <div className='result'>
         <br/>
         <h2>Keputusan anda <i class="far fa-smile-wink"></i></h2>
-        <p>Kemurungan: {depression()} ({depressCompare(depression())})</p>
-        <p>Stres: {stress()} ({stressCompare(stress())})</p>
-        <p>Anzieti: {anxiety()} ({anxietyCompare(anxiety())})</p>
+        <p>Kemurungan: {this.depression()} ({this.depressCompare(this.depression())})</p>
+        <p>Stres: {this.stress()} ({this.stressCompare(this.stress())})</p>
+        <p>Anzieti: {this.anxiety()} ({this.anxietyCompare(this.anxiety())})</p>
         <br/>
         <div style={{overflowX: "auto"}}>
         <table class="table table-striped">
@@ -171,14 +189,21 @@ function Result(props) {
   </tbody>
 </table>
 </div>
+<br/> <br/>
+    <div class="form-group" align="center">
+    <input value={this.state.email} onChange={(e)=>this.setState({email: e.target.value})} style={{maxWidth: "50%"}}type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+    </div>
+    <button className="btn btn-info btn-md" onClick={this.sendResult}> E-mel saya hasil saya </button>
+    <br/> <br/>
+    <a className="btn btn-sm btn-light" href="mailto: hello@thoughtfull.world">Apa-apa soalan? Jangan ragu untuk menjatuhkan kami e-mel!</a><br/><br/>
     </div>
     } else {
         return <div className='result'>
         <br/>
         <h2>Your result <i class="far fa-smile-wink"></i></h2>
-        <p>Depression: {depression()} ({depressCompare(depression())})</p>
-        <p>Stress: {stress()} ({stressCompare(stress())})</p>
-        <p>Anxiety: {anxiety()} ({anxietyCompare(anxiety())})</p>
+        <p>Depression: {this.depression()} ({this.depressCompare(this.depression())})</p>
+        <p>Stress: {this.stress()} ({this.stressCompare(this.stress())})</p>
+        <p>Anxiety: {this.anxiety()} ({this.anxietyCompare(this.anxiety())})</p>
         <br/>
         <div style={{overflowX: "auto"}}>
         <table class="table table-striped">
@@ -224,8 +249,17 @@ function Result(props) {
   </tbody>
 </table>
 </div>
+    <br/> <br/>
+    <div class="form-group" align="center">
+    <input value={this.state.email} onChange={(e)=>this.setState({email: e.target.value})} style={{maxWidth: "50%"}}type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+    </div>
+    <button className="btn btn-info btn-md" onClick={this.sendResult}> Email Me My Result </button>
+    <br/> <br/>
+    <a className="btn btn-sm btn-light" href="mailto: hello@thoughtfull.world">Any Questions? Feel free to drop us an email!</a><br/><br/>
     </div>
     }
+
+}    
     
 }
 
